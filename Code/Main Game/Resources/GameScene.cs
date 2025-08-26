134ff2975ae62a3b82295a4485b9f245592bc406
DenDrummer <muzikant96@gmail.com>
2025-08-26 21:19:02 +0200

    Add par to GameScene script
    and some extra constructors for the class

--- Code/Main Game/Resources/GameScene.cs
+++ Code/Main Game/Resources/GameScene.cs
@@ -8,11 +8,30 @@ public partial class GameScene : Resource
     public string Name = "";
     [Export]
     public string[] tags = [];
+    [Export(PropertyHint.Range, "1,255")]
+    public byte par = 3;
 
     public GameScene() { }
 
     public GameScene(string name)
     {
         Name = name;
+    }
+
+    public GameScene(PackedScene scene, string name, string[] tags, byte par = 3)
+    {
+        this.scene = scene;
+        Name = name ?? "";
+        this.tags = tags ?? [];
+        this.par = par;
+    }
+
+    public GameScene(Node node, string name, string[] tags, byte par = 3)
+    {
+        scene = new();
+        scene.Pack(node);
+        Name = name ?? "";
+        this.tags = tags ?? [];
+        this.par = par;
     }
 }
